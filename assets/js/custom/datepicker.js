b9a0aeb59ce1cd40187df4e11ab3646bb044af0d
mpiranha <chukwubuezeokeke@gmail.com>
2020-05-30 23:21:44 +0100

    Stat page and few fixes

---
+++ assets/js/custom/datepicker.js
@@ -0,0 +1,5 @@
+$('input[name="daterange"]').daterangepicker({
+    opens: 'right'
+}, function (start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+});
--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -71,11 +71,7 @@ $(window).on('load', function () {
         }
     });
 
-    $('input[name="daterange"]').daterangepicker({
-        opens: 'right'
-    }, function (start, end, label) {
-        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-    });
+
 
     notificationCheckbox.each(function () {
         $(this).on('change', function () {
